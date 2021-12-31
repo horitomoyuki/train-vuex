c5ffd7b9fd26ddfcb754d3ba51180e6a4b71da20
horitomoyuki <eziltom0131@gmail.com>
2022-01-01 02:22:16 +0900

    ListコンポーネントにCardコンポーネント呼び出し

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,15 +4,24 @@
       <p class="list-title">{{ title }}</p>
       <div class="deletelist" @click="removeList">×</div>
     </div>
+    <card v-for="(item, index) in cards"
+          :body="item.body"
+          :key="item.id"
+          :cardIndex="index"
+          :listIndex="listIndex"
+    />
     <card-add :listIndex="listIndex" />
   </div>
 </template>
 
 <script>
 import CardAdd from './CardAdd'
+import Card from './Card'
+
 export default {
   components: {
-    CardAdd
+    CardAdd,
+    Card
   },
   props: {
     title: {
