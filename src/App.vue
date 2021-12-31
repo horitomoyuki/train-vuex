f45a0b0b482ecaa7c2a3e028e81a1f281ad8294b
horitomoyuki <eziltom0131@gmail.com>
2021-12-31 22:23:12 +0900

    Boardコンポーネントの呼び出し

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <board />
   </div>
 </template>
 
 <script>
-
+import Board from './components/Board.vue'
 
 export default {
   name: 'app',
   components: {
-    
+    Board
   }
 }
 </script>
