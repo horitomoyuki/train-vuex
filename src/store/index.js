23805383ad545656cdcc9e332d9e83754a76bc69
horitomoyuki <eziltom0131@gmail.com>
2021-12-31 23:09:49 +0900

    Create index.js

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    lists: [],
+  },
+  mutations: {
+    addlist(state, payload) {
+      state.lists.push({ title: payload.title, cards:[] })
+    },
+  },
+  actions: {
+    addlist(context, payload) {
+      context.commit('addlist', payload)
+    },
+  },
+  getters: {
+  }
+})
