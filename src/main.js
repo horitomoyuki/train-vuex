934c6276cc8443c2dfdc8799f40b1996c93213a7
horitomoyuki <78133210+horitomoyuki@users.noreply.github.com>
2022-01-01 01:17:12 +0900

    Revert "Listコンポーネント"

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
-import './assets/css/styles.css'
+import './assets/css/styles.css' 
 
 Vue.config.productionTip = false
 
 new Vue({
-  store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
