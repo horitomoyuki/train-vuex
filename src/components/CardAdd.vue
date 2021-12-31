231a1833a2fcf0833e749abf5faab0dcae816ada
horitomoyuki <eziltom0131@gmail.com>
2022-01-01 01:20:13 +0900

    Create CardAdd.vue

---
+++ src/components/CardAdd.vue
@@ -0,0 +1,23 @@
+<template>
+  <form class="addcard" @submit.prevent="addCardToList">
+    <input v-model="body"
+           type="text"
+           class="text-input"
+           placeholder="Add new card"
+    />
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      body: '',
+    }
+  },
+  
+}
+</script>
